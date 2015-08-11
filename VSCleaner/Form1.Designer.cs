cd98df0be7cbf55dc4029d5fcf9ef0b58de6ae21
HouRong <nmghr9@gmail.com>
2015-08-11 22:54:02 +0800

    Add Code
    V 0.0.1

---
+++ VSCleaner/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace VSCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "执行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "本程序用于清除由Virsual Studio开发所产生的“bin”，“obj”，“Debug”，\n“packages”文件夹下的所有文件，以及“.sdf”文件，使" +
+    "用時请选择代码目录的\n上级文件夹。";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(14, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(215, 16);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "清除并打包成压缩文件(\"../*.zip\")";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(235, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "仅清除";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 112);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VSCleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ VSCleaner/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace VSCleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton2.Checked = true;
+        }
+
+        static void DirSearch(string fDir)
+        {
+            bool findFile = false;
+            try
+            {
+                foreach (string d in Directory.GetDirectories(fDir))
+                {
+                    string[] dStrings = d.Split('\\');
+                    string folderName = dStrings[dStrings.Length - 1];
+                    if (folderName == "obj" || folderName == "bin" || folderName == "packages" || folderName == "Debug")
+                    {
+                        DeleteFolder(d);
+                        findFile = true;
+                    }
+                    if (findFile)
+                    {
+                        Directory.CreateDirectory(d);
+                    }
+                    if (!findFile)
+                    {
+                        DirSearch(d);
+                    }
+                    findFile = false;
+                }
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message);
+            }
+        }
+
+        static void FindSdfFile(string fDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(fDir))
+                {
+                    foreach (string f in Directory.GetFiles(d, "*.sdf"))
+                    {
+                        File.Delete(f);
+                    }
+                    FindSdfFile(d);
+                }
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        static void DeleteFolder(string fDir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFileSystemEntries(fDir))
+                {
+                    if (File.Exists(f))
+                    {
+                        File.Delete(f);
+                    }
+                    else
+                    {
+                        DeleteFolder(f);
+                    }
+                }
+                Directory.Delete(fDir);
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (textBox1.Text != "")
+                {
+                    DirSearch(textBox1.Text);
+                    FindSdfFile(textBox1.Text);
+                }
+                else
+                {
+                    MessageBox.Show("请选择文件夹");
+                }
+
+                try
+                {
+                    string zipFilePath = textBox1.Text;
+                    zipFilePath += string.Format("({0:yyyy-MM-dd H-mm}).zip", DateTime.Now);
+                    ZipFile.CreateFromDirectory(textBox1.Text, zipFilePath);
+                    MessageBox.Show("打包完成，文件为" + zipFilePath);
+                }
+                catch(Exception except)
+                {
+                    MessageBox.Show(except.Message);
+                }
+            }
+            else
+            {
+                if (textBox1.Text != "")
+                {
+                    DirSearch(textBox1.Text);
+                    FindSdfFile(textBox1.Text);
+                    MessageBox.Show("清除完成");
+                }
+                else
+                {
+                    MessageBox.Show("请选择文件夹");
+                }
+            }
+        }
+    }
+}
